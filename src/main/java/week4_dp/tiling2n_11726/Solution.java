72472dc154f8a1bd934f2358abdf9015072d123c
JiSoo <jisoo0507@vtex.co.kr>
2025-08-28 16:23:42 +0900

    [WEEK4] 2Xn타일링 O(N) DP 다시 풀이

--- src/main/java/week4_dp/tiling2n_11726/Solution.java
+++ src/main/java/week4_dp/tiling2n_11726/Solution.java
@@ -18,17 +18,19 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine().trim());
+        int MOD = 10007;
 
-        int n = Integer.parseInt(st.nextToken());
+        if (n == 1) { System.out.println(1); return; }
+        if (n == 2) { System.out.println(2); return; }
 
-        int[] dp  = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
 
-        System.out.println(dp[n]%10007);
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+        System.out.println(dp[n]);
     }
 }
