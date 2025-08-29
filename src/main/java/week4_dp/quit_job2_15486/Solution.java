a407fb4d6f8587550b1ae742b2b57f55d77f914f
SoleLeaf <jisoo0507@naver.com>
2025-08-29 21:42:00 +0900

    [WEEK4] 퇴사(15486) O(N) DP

--- src/main/java/week4_dp/quit_job2_15486/Solution.java
+++ src/main/java/week4_dp/quit_job2_15486/Solution.java
@@ -1,46 +1,25 @@
-package main.java.week4_DP.quit_job2_15486;
-/*
-* 오늘부터 N+1일째 되는 날 퇴사
-* 하루에 하나씩 서로 다른 사람의 상담 배정됨
-* T = 상담을 완료하는데 걸리는 시간
-* P = 상담을 했을 때 받을 수 있는 금액
-* 상담을 하는데 필요한 기간은 1일보다 클 수 있기 때문에 모든 상담을 할 수는 없다.
-* N+1일째에는 회사에 없기 때문에 근무일수를 초과하는 일정의 상담은 받을 수 없다.
-* 상담을 적절히 했을 때, 백준이가 얻을 수 있는 최대 수익을 구하는 프로그램
-* 시간 제한 : 2초, 메모리 제한 512
-* 1<=N<=1500000
-* 1<=T<=50 1<=P<=1000
-* 입력 : N, N개의 줄에 T와 P가 공백으로 구분되어서 주어지며, 1일부터 N일까지 순서대로 주어진다.
-* */
-/*
-* 알고리즘 설계 :
-* 예: N+1 크기의 dp배열에서 N번째 배열에서 T는 1만 허용된다.
-* N-i>=T 선택할 수 있는 부분, 이것과 선택하지 않았을 때의 가격 비교 -> 큰 것 취하고 저장하기 price +1
-* N-i<T인 경우에는 continue;
-* dp[0] 반환
-* */
+package main.java.week4_dp.quit_job2_15486;
+
 import java.io.*;
 import java.util.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        int[] T = new int[N+1];
-        int[] P = new int[N+1];
+        int[] T = new int[N];
+        int[] P = new int[N];
         int[] dp = new int[N+1];
 
-        for(int i =1; i<=N; i++){
-            StringTokenizer st1 = new StringTokenizer(br.readLine().trim());
-            T[i] = Integer.parseInt(st1.nextToken());
-            P[i] = Integer.parseInt(st1.nextToken());
+        for(int i =0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
         }
 
         //역순으로 백트래킹 : skip은 그대로 두는 것, 선택한다면 dp[N-j]
         for(int j = N-1; j>=0; j--){
-            if(N-j<T[j]) continue; //상담하는데 필요한 시간이 남은 근무일수보다 크면 PASS
             int next = j + T[j];
             int take = next <= N?P[j]+dp[next]:0;
             int skip = dp[j+1];
